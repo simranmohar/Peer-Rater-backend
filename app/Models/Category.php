b2e457db44ba03024d93723e1e7b7137f00ae3c4
Harry Seo (Jong Hwan Seo) <jseo51@my.bcit.ca>
2022-03-31 15:47:35 -0700

    Insert belongsTo relationship inside Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Survey;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function survey() {
+        return $this->belongsTo(Survey::class);
+    }
 }

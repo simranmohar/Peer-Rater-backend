226745cb5a0b607101fd42e7543585559acb9789
Harry Seo (Jong Hwan Seo) <jseo51@my.bcit.ca>
2022-04-02 00:23:04 -0700

    Added to use $request->has('isInstructor') to get boolean value saved into isInstructor param

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,12 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request)
-    {
+    {   
         $user = User::create([
              'email'    => $request->email,
              'password' => $request->password,
              'name' => $request->name,
+             'isInstructor' => $request->has('isInstructor'),
          ]);
 
         $token = auth()->login($user);

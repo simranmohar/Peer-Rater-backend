ca2420ce213241c2dd5c724b863b3635f9c5e3f3
Harry Seo (Jong Hwan Seo) <jseo51@my.bcit.ca>
2022-04-02 18:11:37 -0700

    Taking out empty space

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function register(Request $request)
-    {   
+    {
         $user = User::create([
              'email'    => $request->email,
              'password' => $request->password,
